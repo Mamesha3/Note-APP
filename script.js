2e9d62ce54e71a16654b1694f81e9dd31b440e19
Mohammed Ahmed <webmame08@gmail.com>
2025-11-17 00:04:35 -0800

    second commit

--- script.js
+++ script.js
@@ -6,13 +6,62 @@ const saveBtn = document.querySelector('#save_btn')
 const titleInp = document.querySelector('#input_title')
 const content = document.querySelector('#note__content')
 const cancel = document.querySelector('#cancel')
+editingLi = null
 renderSaveData()
 
 
 
+
+function dltBtnf(ul, h32, p2, ls) {
+    // create ul for delete and edit btn
+
+    const delt = document.createElement('li')
+    delt.className = 'dlelete'
+    delt.textContent = 'Delete'
+
+    const edit = document.createElement('li')
+    edit.className = 'editCont'
+    edit.textContent = 'Edit'
+    editDelBtn(edit, delt, h32, p2, ls)
+
+    ul.append(edit, delt)
+}
+
+function editDelBtn(edit, dlt, h3s, ps, li) {
+    edit.addEventListener('click', ()=> {
+        editingLi = li
+
+       container.classList.add('shade')
+       addTaskContainer.style.display = 'block'
+
+       titleInp.value = h3s.textContent
+       content.value = ps.textContent
+      
+    })
+    dlt.addEventListener('click', () => {
+       document.querySelector('.delte_request').classList.toggle('displaythis')
+       container.classList.toggle('shade')
+    })
+
+    document.querySelector('#dont').addEventListener('click', () => {
+        document.querySelector('.delte_request').classList.remove('displaythis')
+        container.classList.remove('shade')
+    })
+
+    
+}
+
+function allDltB(button, ul, dlBack) {
+    button.addEventListener('click', () => {      
+        ul.classList.toggle('ulop')
+        dlBack.classList.toggle('backgroundClick')  
+    })
+}
+
 function creatNoteBox(titleInput, textarea, addTime, checked) {
     // creat elements
     const li = document.createElement('li')
+    li.classList = 'old'
     const divTitle = document.createElement('div')
     const h3 = document.createElement('h3')
     const button = document.createElement('button')
@@ -21,21 +70,22 @@ function creatNoteBox(titleInput, textarea, addTime, checked) {
     const ptype = document.createElement('p')
     const h3Type = document.createElement('h3')
     const a = document.createElement('a')
+    const dlBack = document.createElement('div')
+    dlBack.className = 'background'
+    const ul = document.createElement('ul')
+    ul.className = 'dltOption'
 
     // give them class then append them
     divTitle.className = 'title__dltbtn'
     h3.className = 'title'
     h3.textContent = titleInput
     button.id = 'dlt_btn'
-    button.textContent = '🗑️'
+    button.innerHTML = '<i class="fa-solid fa-ellipsis"></i>'
     
+    allDltB(button, ul, dlBack)
 
-    button.addEventListener('click', () => {
-        li.remove()
-        saveData()
-    })
-
-    divTitle.append(h3, button)
+    divTitle.append(ul, h3, button)
+    dltBtnf(ul, h3, p, li)
 
     p.id = 'note_text'
     p.textContent = textarea
@@ -63,7 +113,7 @@ function creatNoteBox(titleInput, textarea, addTime, checked) {
     })
 
     // append all
-    li.append(divTitle, p, a, divDate)
+    li.append(dlBack, divTitle, p, a, divDate)
     taskcontainer.appendChild(li)
 }
 
@@ -150,12 +200,21 @@ saveBtn.addEventListener('click', () => {
    
    if (!ttlInp || !contInp) {
     alert('please fill some note')
-   }else {
+    return
+   }
     container.classList.remove('shade')
     addTaskContainer.style.display = 'none'
+   
+   if (editingLi) {
+      editingLi.querySelector('.title').textContent = ttlInp
+      editingLi.querySelector('#note_text').textContent = contInp
+      editingLi.querySelector('#type').innerHTML = checkedInput
+      saveData()
+      editingLi = null
+      return
+    }
     creatNoteBox(ttlInp, contInp, getTime, checkedInput)
     saveData()
-   }
 })
 
 cancel.addEventListener('click', () => {
